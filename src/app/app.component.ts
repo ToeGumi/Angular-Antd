d4fba5f77643eca484da5f80ef8f01c7c482545d
to.vo@gumiviet.com <tu.vo@gumiviet.com>
2023-02-14 17:27:20 +0700

    build: todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NzButtonSize } from 'ng-zorro-antd/button';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Antd';
+  size: NzButtonSize = 'large';
+
+  allChecked = false;
+  indeterminate = true;
+  todos = [
+    { title: 'todo1', completed: 'false', checked: false },
+    { title: 'todo2', completed: 'false', checked: false },
+    { title: 'todo3', completed: 'true', checked: false }
+  ];
+
+  updateAllChecked(): void {
+    this.indeterminate = false;
+    if (this.allChecked) {
+      this.todos = this.todos.map(item => ({
+        ...item,
+        checked: true
+      }));
+    } else {
+      this.todos = this.todos.map(item => ({
+        ...item,
+        checked: false
+      }));
+    }
+  }
+
+  updateSingleChecked(): void {
+    if (this.todos.every(item => !item.checked)) {
+      this.allChecked = false;
+      this.indeterminate = false;
+    } else if (this.todos.every(item => item.checked)) {
+      this.allChecked = true;
+      this.indeterminate = false;
+    } else {
+      this.indeterminate = true;
+    }
+  }
+
+  cancel(): void {
+    alert('click cancel');
+  }
+
+  confirm(): void {
+    alert('click confirm');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,40 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { EditOutline, CheckOutline, CloseOutline, DeleteOutline } from '@ant-design/icons-angular/icons';
+
+const icons: IconDefinition[] = [EditOutline, CheckOutline, CloseOutline, DeleteOutline ];
+
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzButtonModule,
+    NzPageHeaderModule,
+    NzMenuModule,
+    NzListModule,
+    NzCheckboxModule,
+    NzGridModule,
+    NzPopconfirmModule,
+    NzIconModule.forRoot(icons)
+    
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
